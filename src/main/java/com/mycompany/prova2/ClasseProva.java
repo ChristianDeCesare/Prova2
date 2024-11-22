304b12717a81e04893ce95dd66b4357646aa91b2
lorenzocibellis <l.cibellis@studenti.unisa.it>
2024-11-22 17:23:35 +0100

    aggiunto ciao lorenzo4

---
+++ src/main/java/com/mycompany/prova2/ClasseProva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prova2;
+
+/**
+ *
+ * @author lorenzocibellis
+ */
+public class ClasseProva {
+    
+    private String nome;
+
+    public ClasseProva(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
