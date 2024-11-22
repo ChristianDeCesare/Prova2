1086995a6a93ca857d3fe8cc907b69eb17b30867
lorenzocibellis <l.cibellis@studenti.unisa.it>
2024-11-22 18:11:05 +0100

    Update Main.java

--- src/main/java/com/mycompany/prova2/Main.java
+++ src/main/java/com/mycompany/prova2/Main.java
@@ -20,7 +20,7 @@ public class Main {
           System.out.println("Ciao 3");
     System.out.println("CIao Lore ha funzionato");
     
-    
+     System.out.println("ok");
      System.out.println("CIao Lore ha funzionato 2");
     }
     
